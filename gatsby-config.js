4f18e0a6020d21e3f970edb9c5a2a23a4a068289
nicholastorr <52306328+nicholastorr@users.noreply.github.com>
2022-02-28 15:30:40 -0500

    test build

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,17 +18,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-sharp`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      // Defaults used for gatsbyImageData and StaticImage
+      defaults: {},
+      // Set to false to allow builds to continue on image errors
+      failOnError: false,
+      // deprecated options and their defaults:
+      base64Width: 20,
+      forceBase64Format: `png/jpg/webp`, // valid formats: png,jpg,webp
+      useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+      stripMetadata: false,
+      defaultQuality: 50,
       },
     },
     {
