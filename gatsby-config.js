29ad7c98ffdc70b406694188b02a72bf6aa5dcc7
nicholastorr <52306328+nicholastorr@users.noreply.github.com>
2022-02-28 15:54:05 -0500

    try again: '

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       resolve: `gatsby-source-drupal7`,
       options: {
-       baseUrl: `http://stagingsupply.htm-mbs.com/`,
+       baseUrl: `stagingsupply.htm-mbs.com`,
        apiBase: `restws_resource.json`,
       } 
     },
