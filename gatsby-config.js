915b4176db6d03b5fd6a2280cdd214639602fc9a
nicholastorr <52306328+nicholastorr@users.noreply.github.com>
2022-03-01 12:04:00 -0500

    added https

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,31 +5,12 @@ module.exports = {
     author: `Inkfolio`
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Inkfolio`,
-        short_name: `Inkfolio`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#4dc0b5`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`
-      },
       resolve: `gatsby-source-drupal7`,
       options: {
-       baseUrl: `http://stagingsupply.htm-mbs.com/`,
+       baseUrl: `https://stagingsupply.htm-mbs.com/`,
        apiBase: `restws_resource.json`,
       } 
-    },
-    `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        tailwind: true,
-        purgeOnly: [`src/css/style.css`]
-      }
     },
   /* `gatsby-plugin-offline` */
   ]
