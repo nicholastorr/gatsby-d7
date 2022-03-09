8d3e822a0470b531e3cbd7bcfb92cb42d96122f8
nick <nick.torres213@gmail.com>
2022-03-09 21:14:51 +0000

    styled

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,47 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Checkbox } from 'antd';
+import { UnorderedListOutlined } from '@ant-design/icons';
 
 
 const SidebarContainer = styled.div`
-    width: 290px;
+    margin-right: 20px;
+    width: 260px;
     overflow: auto;
     position: -webkit-sticky;
     position: sticky;
     top: 20px;
 `
 
-const Filter = styled.li`
-    border-bottom: 1px solid black;
+const SubCategories = styled.div`
+    list-style-type: none;
+    border: 1px solid transparent;
+    box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.5);
+    -moz-box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.5);
+    -webkit-box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.5);
+    li {
+        padding: 5px;
+    }
+    span {
+        margin-right: 7px;
+        margin-left: 7px;
+    }
+    li:hover {
+        background-color: #f5f5f5;
+    }
+    li.active {
+        background-color: #f5f5f5;
+    }
 `
-
-export const Sidebar = ({ setProducts, baseProducts }) => {
+export const Sidebar = ({ path, setProducts, baseProducts }) => {
     const [filters, setFilters] = React.useState([]);
     const [click, setClick] = React.useState(false);
-    const [checked, setChecked] = React.useState(false);
+
+    const url = path ? path : '';
 
     const handleClick = () => {
         setClick(!click);
     }
-
-    function test(checkedValues) {
-        console.log(checkedValues);
-    }
-
 
     const onChange = (checkedValues) => {
         
@@ -60,33 +74,35 @@ export const Sidebar = ({ setProducts, baseProducts }) => {
         rollsizes.push({ 'label': `${roll}`, 'value': `${roll}`});
     });
 
+    console.log(url);
 
     return (
         <SidebarContainer>
-            <h3>Mbs Sign Supply</h3>
-                <ul>Sub Categories</ul>
-                    <a href="/vinyls/calendered_vinyl"><li>Calendered Vinyl</li></a>
-                    <li>Cast Vinyl</li>
-            <h3>Filters</h3>
-            {filters.length > 0 ? <button onClick={clearFilters}>Clear Filters</button> : null}
-            <li onClick={() => handleClick()}>Roll Size</li>
-                {/*map through roll size array*/}
-                {/*each size has an onclick function that filters the products array*/}
-                {click ? <Checkbox.Group options={rollsizes} onChange={onChange}/> : null}
-            <li>Width</li>
-            <li>Length</li>
-            <li>Series</li>
-            <li>Color</li>
-            <li>Finish</li>
-            <li>Color Number</li>
-            <li>Color Range</li>
-            <li>Adhesive</li>
-            <li>Application</li>
-            <li>Punched</li>
-            <li>Surface</li>
-            <li>Vinyl Type</li>
-            <li>Durability</li>
-            <li>Vinyl Class</li>
+                <ul><h2 style={{fontWeight: "bold", marginTop: "30px"}}>Sub Categories</h2></ul>
+                <SubCategories>
+                    <a href="/vinyls/calendered_vinyl"><li><UnorderedListOutlined />Calendered Vinyl</li></a>
+                    <a href="/cast-vinyl"><li className="active"><UnorderedListOutlined />Cast Vinyl</li></a>
+                </SubCategories>
+                <h3>Filters</h3>
+                {filters.length > 0 ? <button onClick={clearFilters}>Clear Filters</button> : null}
+                    <li onClick={() => handleClick()}>Roll Size</li>
+                    {/*map through roll size array*/}
+                    {/*each size has an onclick function that filters the products array*/}
+                    {click ? <Checkbox.Group options={rollsizes} onChange={onChange}/> : null}
+                    <li>Width</li>
+                <li>Length</li>
+                <li>Series</li>
+                <li>Color</li>
+                <li>Finish</li>
+                <li>Color Number</li>
+                <li>Color Range</li>
+                <li>Adhesive</li>
+                <li>Application</li>
+                <li>Punched</li>
+                <li>Surface</li>
+                <li>Vinyl Type</li>
+                <li>Durability</li>
+                <li>Vinyl Class</li>
         </SidebarContainer>
     )
 }
